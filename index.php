d95abab82e504c49c1d1fbd237a36187a781bc36
unknown <ashanker@q3gn0367.q3tech.net>
2016-01-19 12:51:11 +0530

    add index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="assets/js/app.js"></script>
+</head>
+<body>
+ 
+<form action="/" id="searchForm">
+  <input type="text" name="s" placeholder="Search...">
+  <input type="submit" value="Search">
+</form>
+<!-- the result of the search will be rendered inside this div -->
+<div id="result"></div>
+  <?php echo 'ajay shanker'; ?>
+</body>
+</html>
